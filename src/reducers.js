cf2c92d7e70739671227083155681decc4c74f23
sivachandran06 <sivachandran997@gamil.com>
2024-05-12 10:30:27 +0530

    first commit

--- src/reducers.js
+++ src/reducers.js
@@ -1,6 +1,27 @@
 import { v4 as uuidv4 } from "uuid";
 
 const reducer = (state, action) => {
+    switch (action.type){
+        case "add":
+        return{
+            ...state,
+            user:[
+                ...state.user,
+                {id:uuidv4(),name: action.name}
+            ],
+            balance: state.balance + 10
+        };
+        case "fuel":
+        if (state.balance >=50){
+            return{
+                ...state,
+                balance: state.balance - 50
+          };
+        }
+        return state;
+        default:
+            return state;
+    }
 };
 
 export default reducer;
